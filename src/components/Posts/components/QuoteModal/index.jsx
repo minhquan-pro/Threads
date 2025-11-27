f9d08ca83b884a9e939c8fbbda7b9298804185e5
Lê Minh Quân <minhquan0963580006@gmail.com>
2025-11-27 09:18:37 +0700

    refactor: QuoteModal to streamline post quoting

--- src/components/Posts/components/QuoteModal/index.jsx
+++ src/components/Posts/components/QuoteModal/index.jsx
@@ -1,5 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useContext, useState } from "react";
 import { toast } from "react-toastify";
 import { Image } from "lucide-react";
 
@@ -12,45 +11,27 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
-import { getPostById, quotePost as quotePostService } from "@/services/Posts";
+import { quotePost as quotePostService } from "@/services/Posts";
 import { useCurrentUser } from "@/features/auth";
 import UserProfileDialog from "@/components/UserProfileDialog";
 import Loading from "@/components/Loading";
-import FeedItem from "@/components/FeedItem";
 import PostHeader from "../PostHeader";
 import { PermissionContext } from "@/context/PermissionContext";
+import QuoteItem from "@/components/QuoteItem";
+import UserAvatar from "@/components/UserAvatar";
+import ThreadLine from "@/components/ThreadLine";
 
-const QuoteModal = ({ postId, isOpen, onClose }) => {
-  const [quotePost, setQuotePost] = useState(null);
+const QuoteModal = ({ post, postId, isOpen, onClose }) => {
+  const currentUser = useCurrentUser();
   const [loading, setLoading] = useState(false);
   const [content, setContent] = useState("");
   const { permission } = useContext(PermissionContext);
-
-  const dispatch = useDispatch();
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    if (!isOpen || !postId) return;
-
-    (async () => {
-      setLoading(true);
-      try {
-        const response = await getPostById(postId);
-        setQuotePost(response);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, [dispatch, postId, isOpen, onClose]);
 
   const handleChangeContent = (e) => {
     setContent(e.target.value);
   };
 
   const handleClose = () => {
-    setQuotePost(null);
     onClose();
     setContent("");
   };
@@ -88,36 +69,45 @@ const QuoteModal = ({ postId, isOpen, onClose }) => {
             <Button
               onClick={onClose}
               variant="outline"
-              className="border-none shadow-none"
+              className="text-md border-none shadow-none"
             >
               Hủy
             </Button>
             <DialogTitle className="m-auto">Thread mới</DialogTitle>
           </div>
         </DialogHeader>
-        <div className="flex w-full gap-3 p-2">
-          <UserProfileDialog user={currentUser} />
-          <div className="w-full">
-            <PostHeader user={currentUser} hideDate />
-            <input
-              className="w-full border-none p-0 text-sm shadow-none outline-none placeholder:text-gray-600"
-              placeholder="Hãy chia sẻ suy nghĩ của bạn..."
-              value={content}
-              onChange={handleChangeContent}
-            />
-            <div className="mt-3 flex items-center gap-3">
-              <Image color="gray" size={20} />
+        <div>
+          <div className="flex w-full gap-3">
+            <div className="relative">
+              <ThreadLine show />
+              <UserProfileDialog user={currentUser} />
             </div>
+            <div className="w-full">
+              <PostHeader user={currentUser} hideDate />
+              <input
+                className="w-full border-none p-0 text-sm shadow-none outline-none placeholder:text-gray-600"
+                placeholder="Hãy chia sẻ suy nghĩ của bạn..."
+                value={content}
+                onChange={handleChangeContent}
+              />
+              <div className="mt-3 flex items-center gap-3">
+                <Image color="gray" size={20} />
+              </div>
 
-            <div className="mt-5 w-full rounded-md border border-gray-300 p-3 shadow">
-              {loading ? (
-                <div className="flex justify-center">
-                  <Loading size={"w-6 h-6"} />
-                </div>
-              ) : (
-                quotePost && <FeedItem post={quotePost} variant="quote" />
-              )}
+              <div>
+                {loading ? (
+                  <div className="flex justify-center">
+                    <Loading size={"w-6 h-6"} />
+                  </div>
+                ) : (
+                  post && <QuoteItem quotedPostId={post.id} quotedPost={post} />
+                )}
+              </div>
             </div>
+          </div>
+          <div className="mt-4 flex items-center gap-2 pl-3">
+            <UserAvatar imgSize="w-4 h-4" />
+            <div className="text-sm text-gray-500">Thêm vào threads</div>
           </div>
         </div>
         <DialogFooter>
