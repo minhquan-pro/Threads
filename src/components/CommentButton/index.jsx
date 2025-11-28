ca8f900f5157ed6fd018902925a6502bfff19461
Lê Minh Quân <minhquan0963580006@gmail.com>
2025-11-28 15:39:13 +0700

    feat: enhance CommentButton and

--- src/components/CommentButton/index.jsx
+++ src/components/CommentButton/index.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import { MessageCircle } from "lucide-react";
 import Interactions from "../Interactions";
+import ReplyModal from "../ReplyModal";
 
-const CommentButton = ({ count }) => {
-  const handleComment = () => {};
+const CommentButton = ({ post }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleComment = (e) => {
+    e.stopPropagation();
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
   return (
-    <Interactions
-      count={count}
-      Icon={MessageCircle}
-      onClick={handleComment}
-      title="Đăng ký để trả lời"
-      description="Chỉ còn một bước nữa là bạn có thể tham gia cuộc trò chuyện rồi."
-    />
+    <>
+      <Interactions
+        count={post.replies_count}
+        Icon={MessageCircle}
+        onClick={handleComment}
+        title="Đăng ký để trả lời"
+        description="Chỉ còn một bước nữa là bạn có thể tham gia cuộc trò chuyện rồi."
+      />
+
+      <ReplyModal post={post} isOpen={isOpen} onClose={handleClose} />
+    </>
   );
 };
 export default CommentButton;
