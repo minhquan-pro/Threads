f6831103250d51e5d40f4fb407eaaf612fd17e1e
Lê Minh Quân <minhquan0963580006@gmail.com>
2025-12-05 10:10:39 +0700

    chore: update package.json for deployment and add new dependencies; configure Vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			// eslint-disable-next-line no-undef
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/Threads/",
 });
