913f07956fd4155e53b5a67551e6463fc8effd05
Lê Minh Quân <minhquan0963580006@gmail.com>
2025-12-19 16:16:12 +0700

    feat: implement parent post fetching logic in ItemDetailPage

--- src/pages/ItemDetail/index.jsx
+++ src/pages/ItemDetail/index.jsx
@@ -1,30 +1,26 @@
-import { useLocation, useNavigate, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
 
+import { fetchPostById } from "@/services/Posts";
 import { selectItemById, selectLoadingById } from "@/features/posts";
 import FeedItem from "@/components/Posts/FeedItem";
 import Loading from "@/components/common/Loading";
 import ThreadLine from "@/components/common/ThreadLine";
 import CommentSection from "./components/CommentSection";
 import ActivityHeader from "@/components/navigation/ActivityHeader";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchPostById } from "@/services/Posts";
 
 const ItemDetailPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const location = useLocation();
-  const parentId = location.state?.parentId;
   const { postId } = useParams();
-
-  const parentItem = useSelector((state) => selectItemById(state, parentId));
-  const parentPostLoading = useSelector((state) =>
-    selectLoadingById(state, parentId),
-  );
+  const [parentChain, setParentChain] = useState([]);
+  const [loadingParents, setLoadingParents] = useState(false);
   const currentItem = useSelector((state) => selectItemById(state, postId));
   const currentItemLoading = useSelector((state) =>
     selectLoadingById(state, postId),
   );
+  const allPosts = useSelector((state) => state.posts.byId);
 
   // Fetch current post
   useEffect(() => {
@@ -33,12 +29,42 @@ const ItemDetailPage = () => {
     }
   }, [postId, currentItem, currentItemLoading, dispatch]);
 
-  // Fetch parent post
   useEffect(() => {
-    if (parentId && !parentItem && !parentPostLoading) {
-      dispatch(fetchPostById(parentId));
-    }
-  }, [parentId, parentItem, parentPostLoading, dispatch]);
+    const fetchParentChain = async () => {
+      if (!currentItem?.parent_id) {
+        setParentChain([]);
+        return;
+      }
+
+      setLoadingParents(true);
+      const chain = [];
+      let currentParentId = currentItem.parent_id;
+
+      try {
+        while (currentParentId) {
+          if (!allPosts[currentParentId]) {
+            await dispatch(fetchPostById(currentParentId)).unwrap();
+          }
+
+          const parentPost = allPosts[currentParentId];
+
+          if (parentPost) {
+            chain.unshift(parentPost);
+            currentParentId = parentPost.parent_id;
+          } else {
+            break;
+          }
+        }
+        setParentChain(chain);
+      } catch (error) {
+        console.error("Error fetching parent chain:", error);
+      } finally {
+        setLoadingParents(false);
+      }
+    };
+
+    fetchParentChain();
+  }, [currentItem?.parent_id, dispatch, allPosts]);
 
   // Handle deleted post
   useEffect(() => {
@@ -67,7 +93,7 @@ const ItemDetailPage = () => {
     );
   }
 
-  // Post not found (after loading completed)
+  // Post not found
   if (!currentItem && !currentItemLoading) {
     return (
       <div className="flex justify-center py-10">
@@ -87,26 +113,24 @@ const ItemDetailPage = () => {
     <div className="min-h-screen pt-4">
       {/* Post detail */}
       <div className="ps-6 pe-6">
-        {parentId ? (
+        {currentItem?.parent_id ? (
           <div>
-            {parentPostLoading ? (
+            {loadingParents ? (
               <div className="flex justify-center py-4">
                 <Loading size="w-5 h-5" />
               </div>
-            ) : parentItem ? (
+            ) : (
               <>
-                <div className="relative">
-                  <ThreadLine show />
-                  <FeedItem post={parentItem} disableNavigation />
-                </div>
+                {parentChain.map((parent) => (
+                  <div key={parent.id} className="relative">
+                    <ThreadLine show />
+                    <FeedItem post={parent} disableNavigation />
+                  </div>
+                ))}
                 <div className="mt-3">
                   <FeedItem post={currentItem} disableNavigation />
                 </div>
               </>
-            ) : (
-              <div className="py-4 text-center text-sm font-bold text-gray-500 dark:text-gray-400">
-                Không thể tải bài viết gốc
-              </div>
             )}
           </div>
         ) : (
