f930d1e284e6f798106a4f8e5db3db784781739a
Lê Minh Quân <minhquan0963580006@gmail.com>
2026-01-08 07:51:24 +0700

    feat: add mobile tabs for navigation in Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { Link, useLocation } from "react-router";
 
 import { getPosts } from "@/services/Posts";
 import { useCurrentUser } from "@/features/auth";
@@ -12,6 +13,9 @@ import Posts from "@/components/Posts";
 const Home = () => {
   const dispatch = useDispatch();
   const currentUser = useCurrentUser();
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+  const isFollowing = location.pathname.startsWith("/following");
   const posts = useSelector(selectList);
   const loading = useSelector(selectLoadingAllPost);
 
@@ -31,8 +35,36 @@ const Home = () => {
   }, [currentUser, dispatch, posts.length]);
 
   return (
-    <div className="overflow-hidden bg-white pt-16 dark:bg-[#181818]">
+    <div className="overflow-hidden bg-white pt-14 dark:bg-[#181818]">
       {currentUser && <CreatePost />}
+
+      {/* Mobile Tabs: For You / Following */}
+      {currentUser && (
+        <div className="border-b border-gray-200 bg-white md:hidden dark:border-[#2f2f2f] dark:bg-black">
+          <div className="mx-auto flex max-w-screen-sm">
+            <Link
+              to="/"
+              className={`${
+                isHome
+                  ? "border-b-2 border-black text-black dark:border-white dark:text-white"
+                  : "text-gray-500 dark:text-gray-400"
+              } flex-1 py-2 text-center font-semibold`}
+            >
+              Dành cho bạn
+            </Link>
+            <Link
+              to="/following"
+              className={`${
+                isFollowing
+                  ? "border-b-2 border-black text-black dark:border-white dark:text-white"
+                  : "text-gray-500 dark:text-gray-400"
+              } flex-1 py-2 text-center font-semibold`}
+            >
+              Đang theo dõi
+            </Link>
+          </div>
+        </div>
+      )}
 
       {loading && posts.length <= 3 ? (
         <PostSkeleton count={10} />
