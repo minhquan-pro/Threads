43cb5c05d62a75cfb332f5dfe17d87e5aecfad86
Lê Minh Quân <minhquan0963580006@gmail.com>
2026-01-09 23:29:43 +0700

    feat: add imageOverlay state management in HomeTabs for dynamic z-index adjustment

--- src/pages/Home/components/HomeTabs/index.jsx
+++ src/pages/Home/components/HomeTabs/index.jsx
@@ -2,8 +2,35 @@ import CurvedBorderBottom from "@/components/common/CurvedBorderBottom";
 import SnowOverlay from "@/components/common/SnowOverlay";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TAB_VALUES } from "@/constants";
+import { useEffect, useState } from "react";
 
 const HomeTabs = ({ children, currentTab, handleValueChange }) => {
+  const [imageOverlay, setImageOverlay] = useState(
+    document.body.classList.contains("imageOverlay"),
+  );
+
+  useEffect(() => {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === "class") {
+          const hasImageOverlay =
+            document.body.classList.contains("imageOverlay");
+          setImageOverlay(hasImageOverlay);
+          console.log("imageOverlay class:", hasImageOverlay);
+        }
+      });
+    });
+
+    observer.observe(document.body, {
+      attributes: true, // Theo dõi thay đổi attributes
+      attributeFilter: ["class"], // Chỉ theo dõi attribute "class"
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <Tabs
@@ -11,7 +38,9 @@ const HomeTabs = ({ children, currentTab, handleValueChange }) => {
         value={currentTab}
         onValueChange={handleValueChange}
       >
-        <div className="fixed top-0 right-0 left-0 z-50 m-auto hidden w-[700px] md:block">
+        <div
+          className={`fixed top-0 right-0 left-0 ${imageOverlay ? "z-10" : "z-50"} m-auto hidden w-[700px] md:block`}
+        >
           <div className="flex items-center">
             <TabsList className="relative min-h-16 w-full gap-5 rounded-none border-gray-800 bg-white dark:border-[#323030] dark:bg-black">
               <TabsTrigger value="for-you">Dành cho bạn</TabsTrigger>
