2bce6d7901fff557ad4a8bf03a4cbd24a725dfb1
Lê Minh Quân <minhquan0963580006@gmail.com>
2026-01-25 06:03:13 +0700

    feat: add fake posts data and integrate FeedItem component in Profile page

--- src/pages/FollowingFeed/index.jsx
+++ src/pages/FollowingFeed/index.jsx
@@ -4,9 +4,19 @@ import FeedItem from "@/components/Posts/FeedItem";
 import { useIsDesktop } from "@/hooks";
 
 // DemoVideo
-import videoDemo3 from "/videosDemo/video-3.mp4";
 import videoDemo5 from "/videosDemo/video-5.mp4";
 import videoDemo6 from "/videosDemo/video-6.mp4";
+
+const demoImages = [
+  "https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=800",
+  "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=800",
+  "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=800",
+  "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=800",
+  "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=800",
+  "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?w=800",
+  "https://images.unsplash.com/photo-1565958011703-44f9829ba187?w=800",
+  "https://images.unsplash.com/photo-1529042410759-befb1204b468?w=800",
+];
 
 // Dữ liệu demo
 const DEMO_POSTS = [
@@ -15,7 +25,7 @@ const DEMO_POSTS = [
     user: {
       id: 116,
       username: "ygt1016",
-      name: "Quý Đặng",
+      name: "ygt1016",
       avatar_url:
         "https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/612455415_122283125516036290_1325764938759042572_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=833d8c&_nc_ohc=JG6QAg7_9SQQ7kNvwFWGKny&_nc_oc=AdkvHw1fune71ciWZo6_B9DYec38X0eEQ-K9r3gzdEMGpgEwfYVYNcG7JIm4i6DjpIA&_nc_zt=23&_nc_ht=scontent.fhan18-1.fna&_nc_gid=hFPrwyO2p1FpWCLq9nJCaw&oh=00_AfpqbX44186FQ6ZnU6JMcXrfflG-vthVB_skkQ85JUlMYw&oe=696825F0",
       isFollowing: true,
@@ -30,120 +40,103 @@ const DEMO_POSTS = [
     is_liked_by_auth: true,
   },
   {
-    id: 25789,
+    id: 26590,
     user: {
-      id: 116,
-      username: "quydang2905",
-      name: "Quý Đặng",
+      id: 117,
+      username: "foodie_saigon",
+      name: "Sài Gòn Ăn Vặt",
       avatar_url:
-        "https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/292600939_115228927905082_8122933722667759685_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=vM9lr8VO4noQ7kNvwE_4-Rq&_nc_oc=AdnjBKBtTOymDBNdZctEw56tBCNddKAjvBSQesLj9dIvPa7uHzYnox2qQM6QvnxbE64&_nc_zt=23&_nc_ht=scontent.fhan18-1.fna&_nc_gid=IS_iobiOuneJ6YTL06_Zgg&oh=00_AfppcwB0xDRJ3_l3vakZXNix6d8sb7R0aGHcfpNAyFJnmQ&oe=6967AE9A",
-      isFollowing: true,
-      bio: `HUE - TOKYO
-Mindset – Healing – Growth`,
-    },
-    content: `Nghe này...
-Chỉ có duy nhất 1 con đường
-dẫn bạn tới TỰ DO,
-Chính là HỌC TẶP!`,
-    media_urls: [videoDemo3],
-    likes_count: 160,
-    reposts_and_quotes_count: 14,
-    replies_count: 23,
-    created_at: "4h ago",
-    is_liked_by_auth: true,
-  },
-  {
-    id: 1231,
-    user: {
-      id: 102,
-      username: "ThePresentWriter",
-      name: "The Present Writer",
-      avatar_url:
-        "https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/431339708_979983020158373_7980601288300704641_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=g5EJAKjJDG0Q7kNvwFr2eXL&_nc_oc=AdlBWX4exd8ZrgNDSXQ65omzAzYNDlUS4ebYOUewskl6cWowHZjojPiRQgRSlzKK6tA&_nc_zt=23&_nc_ht=scontent.fhan18-1.fna&_nc_gid=cg341Bzbi5CoTKc47t2SHQ&oh=00_AfoeK4lz2MhGh2ZFX9KxTtUZ476hfMvjqrpsB-r_TnwgGQ&oe=6966CE2A",
-      isFollowing: true,
-    },
-    content: `Hai lần đầu tiên tới Sài Gòn dạy cho mình rằng: Không có gì trên đời là tuyệt đối, là duy nhất, là một đi không trở lại. Nếu mình nỗ lực cố gắng, từng bước đi lên, làm hết khả năng của mình, cộng với một chút may mắn và ủng hộ từ những người thân yêu, mình sẽ đạt được điều mà mình mong muốn. 
-
-Cảm ơn Sài Gòn vì bài học tuổi trẻ này nhé!`,
-    media_urls: [
-      "https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/597075298_1420638689426135_8723927384913540251_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=hHm8XhxH4BUQ7kNvwF59A8A&_nc_oc=AdnDAQb3JM4eXzOaoxuVBA57E57OtLXRqK79MSF1vZHKnUvngXfofW624aWziReYP5c&_nc_zt=23&_nc_ht=scontent.fhan18-1.fna&_nc_gid=po1T1XdwQjfSzAJeFR3rCQ&oh=00_Afo3OhA7kNw96rJLt6Sew1dZPXKSrbauih7ZD1dd_RaIJA&oe=6966E9E8",
-      "https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/597554482_1420638652759472_2347356773546397577_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=-hirFtaXMuwQ7kNvwFKjhOp&_nc_oc=AdkdRQ4QFiuODIQsocgwUv0T5b9o58DrZLGV5tNaAJGZgjqtk_TKGuPLtxmL5D5FAFg&_nc_zt=23&_nc_ht=scontent.fhan18-1.fna&_nc_gid=JO5uCmobo9onQ2p8-DEc8w&oh=00_AfozngfHRrtOBpPMnCwDoNEb8H9hezkXxRm1lFmT-qAwJA&oe=6966F193",
-      "https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/595232847_1420638709426133_8101040095090563629_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=cZHZ45ImYXEQ7kNvwGvAFHo&_nc_oc=AdlXhaTJgU_RRsxCbrEPIQAa4rpj_uXAg0Z-CKnmclnUcnbEetEo7YeX5mnJf41JCXE&_nc_zt=23&_nc_ht=scontent.fhan18-1.fna&_nc_gid=bRRb83coVDK-f45PKRuM1g&oh=00_Afpjb9z666VM19_Jf5Z3IPtjImStBVBvj513Pwt5anHscA&oe=6966E795",
-      "https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/596798741_1420638809426123_302319853754569111_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=lT8sKhP5qloQ7kNvwFUKJNM&_nc_oc=AdmO5F_7Ge64QMhRVK0cTJuxTewHxtBwl-I-IYKImd9I3oZ3dc4G2watlxN88SqTyNs&_nc_zt=23&_nc_ht=scontent.fhan18-1.fna&_nc_gid=cehofCDhmBvUQpgea8gTnQ&oh=00_AfotEc1YTDbK_o8Q-sI247vWazvtGF211XnsFlXs10TQKg&oe=6966ED66",
-    ],
-    likes_count: 1300,
-    reposts_and_quotes_count: 25,
-    replies_count: 19,
-    created_at: "2026-01-03T03:17:18.000000Z",
-    is_liked_by_auth: true,
-  },
-  {
-    id: 100,
-    user: {
-      id: 108,
-      username: "DogeDesigner",
-      name: "@cb_doge",
-      avatar_url:
-        "https://pbs.twimg.com/profile_images/1498070100393754625/C2V-fbll_400x400.jpg",
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=200",
+      isFollowing: false,
       verified: true,
-      isFollowing: true,
     },
     content:
-      "BREAKING: xAI is investing more than $20 billion in Mississippi to build a massive data center, called 'MACROHARDRR' This 800,000 sqft facility will host the world’s largest supercomputer & mark the largest investment in state history.xAI is the fastest growing AI company.",
-    media_urls: [],
-    likes_count: 18945,
-    reposts_and_quotes_count: 723,
-    replies_count: 3456,
-    created_at: "2026-01-08T03:17:18.000000Z",
-    is_liked: false,
+      "Phở bò Hà Nội chuẩn vị! Nước dùng trong vắt, thịt mềm tan trong miệng 🍜✨",
+    media_urls: [demoImages[0], demoImages[1], demoImages[2]],
+    likes_count: 3420,
+    reposts_and_quotes_count: 128,
+    replies_count: 89,
+    created_at: "2h ago",
+    is_liked_by_auth: false,
   },
   {
-    id: 12351,
+    id: 26591,
     user: {
-      id: 102,
-      username: "gilly.nah",
-      name: "nguyen quynh anh",
+      id: 118,
+      username: "chef_minh",
+      name: "Chef Minh Nguyen",
       avatar_url:
-        "https://instagram.fhan18-1.fna.fbcdn.net/v/t51.75761-15/485011407_18054813161471259_8388842248772277725_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&_nc_cat=110&ig_cache_key=MzU4OTQ5ODkzOTcyMjU5MDI1Nw%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTgwMC5zZHIuQzMifQ%3D%3D&_nc_ohc=syW730curPMQ7kNvwFvPIvw&_nc_oc=Adl-qacLI77DujzDlPwViS_AhWuxELiXEBald5VW-XKQm-hoM6A12_vnNo_AadDjE2Q&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan18-1.fna&_nc_gid=MyJgRtbXoFRWnktwQXG_Ww&oh=00_Afo8VuXyQAnTaoIScLLuaTARcPlQfjeWsBzIcUA1jA5Vvg&oe=6966E298",
+        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=200",
       isFollowing: true,
-    },
-    content: `xem xong “Chúng ta của 8 năm sau” thấy yêu Hà Nội quá nên quyết tâm sang bên TT Thành Công xem nó có giống như trong phim không.
-may sao rủ được cạ cứng đi cùng`,
-    media_urls: [
-      "https://instagram.fhan18-1.fna.fbcdn.net/v/t51.82787-15/583145827_18080746646471259_617465477237827889_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=109&ig_cache_key=Mzc2NzA2NzgyMzMxNjI5MjEwNQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTkxMC5zZHIuQzMifQ%3D%3D&_nc_ohc=rFOVlxZNUAMQ7kNvwErc6z3&_nc_oc=AdngCXscO-zI9dfYmxisWI3PPGWLC1F_5h-qSSdftPij_4bK71hLPOtr98Ubm4RabqM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan18-1.fna&_nc_gid=MyJgRtbXoFRWnktwQXG_Ww&oh=00_AfpjyB2LpMarg-b0ouHmerMJI4Ku7ciOWGLIaozdJAo_1Q&oe=6966C1E9",
-      "https://instagram.fhan18-1.fna.fbcdn.net/v/t51.82787-15/581097436_18080746655471259_3911729327776037949_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&_nc_cat=106&ig_cache_key=Mzc2NzA2NzgyMzQ0MjE1NzIwNA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTkxMC5zZHIuQzMifQ%3D%3D&_nc_ohc=dH4dqFz_rasQ7kNvwHJophr&_nc_oc=AdnGW6Ikr3gsxyZL6hRTnrYQ-iRVypk_lk9YFJXTHd83DgXyzToJg78d8brFiINH9XQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan18-1.fna&_nc_gid=MyJgRtbXoFRWnktwQXG_Ww&oh=00_AfqB_RcJg___85JYclH5arhBon7Zd85EnQCs_DhFwqDxtA&oe=6966DAE7",
-      "https://instagram.fhan18-1.fna.fbcdn.net/v/t51.82787-15/583197128_18080746664471259_5463650762918122673_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&_nc_cat=110&ig_cache_key=Mzc2NzA2NzgyMzMyNDY4MzEyMA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTkxMC5zZHIuQzMifQ%3D%3D&_nc_ohc=fUSJ8GrCRLkQ7kNvwE9a-Iw&_nc_oc=AdlSG79fIrOu2orHJZPrlmoT1ZPWPOnK3ryWcZCSv1Ok7eJslj1LzWW97iehwcSpWbM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan18-1.fna&_nc_gid=MyJgRtbXoFRWnktwQXG_Ww&oh=00_Afod9GjZjZvCtOfiQ7AU3olGG_1PHbOFeaPO46sTRFFvCQ&oe=6966D813",
-      "https://instagram.fhan18-1.fna.fbcdn.net/v/t51.82787-15/583705172_18080746682471259_599856935436099776_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&_nc_cat=105&ig_cache_key=Mzc2NzA2NzgyMzYyNjY3NzMxOA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTkxMC5zZHIuQzMifQ%3D%3D&_nc_ohc=CnR2tJpsvZYQ7kNvwGvrKB7&_nc_oc=AdlX1lpt0Ly97Alipl05V9___Zpv8fJ6xnIhLYtukSu7KvgAZMf-IeArpMCeHnAv4-c&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan18-1.fna&_nc_gid=MyJgRtbXoFRWnktwQXG_Ww&oh=00_AfpFP6GR_DDnDDJ8vz01WAUEcacb70pUzrnpLtQQK1Awew&oe=6966F45F",
-      "https://instagram.fhan18-1.fna.fbcdn.net/v/t51.82787-15/582870855_18080746691471259_467208734127455070_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&_nc_cat=101&ig_cache_key=Mzc2NzA2NzgyMzMyNDY3Njk5MQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTkxMC5zZHIuQzMifQ%3D%3D&_nc_ohc=pHcbVJeLA_0Q7kNvwEx0cqM&_nc_oc=AdkbyUPiOqyFLKLrbFPDj36Foh0EM7hTrJjUSgiPZKtWx2ZWM6gH_-f8IN-sgRXy_G4&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan18-1.fna&_nc_gid=MyJgRtbXoFRWnktwQXG_Ww&oh=00_Afq53DsxiL3U01iLj9B1adlhQsYqYB_xcSnC9EGb-eEe_w&oe=6966C715",
-    ],
-    likes_count: 189,
-    reposts_and_quotes_count: 4,
-    replies_count: 26,
-    created_at: "2025-12-05T03:17:18.000000Z",
-    is_liked_by_auth: true,
-  },
-  {
-    id: 18,
-    user: {
-      id: 102,
-      username: "sontungmtp",
-      name: "Sơn Tùng M-TP 💋",
-      avatar_url:
-        "https://5sfashion.vn/storage/upload/images/ckeditor/4KG2VgKFDJWqdtg4UMRqk5CnkJVoCpe5QMd20Pf7.jpg",
       verified: true,
-      isFollowing: true,
     },
     content:
-      "Nhiều ông bà đến tận giờ này vẫn còn thắc mắc … Sao ông Tùng hứa đi chạy bộ HỒ GƯƠM mà không thấy đâuuu … 🙂Đây … đây này … Đã muốn cất giấu kí ức này đi mà cứ bắt người ta phải moi lên mới chịuuuu … 🙃Có biết hôm đấy chạy bộ một mình quanh hồ lạnh lẽo thế nào khôngggg 🥲🥲🥲",
-    media_urls: [
-      "https://instagram.fhan19-1.fna.fbcdn.net/v/t51.82787-15/583535882_18541006435042044_7868208800947324188_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=100&ig_cache_key=Mzc2NjkyMDgyODIyODM3MzM3Mw%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTc5NS5zZHIuQzMifQ%3D%3D&_nc_ohc=qYfg57qzB6sQ7kNvwH4iLYS&_nc_oc=AdmHiNEQbv3ruH1HxeTQV-Dt0K-ROPDJLISHx4oAJcV5XN7ZE3E35jk0FPUwjb6GKmQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan19-1.fna&_nc_gid=qNoLOBfVycuQmYKBwoE-Ew&oh=00_Afosy4FdXit74BIN1kF0C8tT4hyQm-27DAmdgBofz3-hZg&oe=69668874",
-      "https://instagram.fhan19-1.fna.fbcdn.net/v/t51.82787-15/581435984_18541006444042044_2102571369351295835_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&_nc_cat=109&ig_cache_key=Mzc2NjkyMDgyODE3ODAyOTk2Ng%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTc5NS5zZHIuQzMifQ%3D%3D&_nc_ohc=Ec3iH89s78YQ7kNvwHwwHvk&_nc_oc=Adm2aBPd0mV41asqCnPIRTICesId4h5hitzO08iLJ6BMYzf8E15MOrUPxSzrYS1dkl8&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan19-1.fna&_nc_gid=qNoLOBfVycuQmYKBwoE-Ew&oh=00_AfqHukTb5dPEAJY526KituDUlti_4USXOos9cbWZX-zgLw&oe=69667047",
-      "https://instagram.fhan19-1.fna.fbcdn.net/v/t51.82787-15/581725970_18541006453042044_1118421191812529200_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=101&ig_cache_key=Mzc2NjkyMDgyODE3ODA1MTA0Ng%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTc5NS5zZHIuQzMifQ%3D%3D&_nc_ohc=OijlxBiEUMwQ7kNvwE86RAI&_nc_oc=Admj5r8lNQPURZTtx8bOTo8ogv8-0-A3D1HU0mOnpm-SkdPr_WynhSHVtZ0hsgfWz-0&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fhan19-1.fna&_nc_gid=qNoLOBfVycuQmYKBwoE-Ew&oh=00_AfoTQayyX2YtiMQQrxS8XBE0Iwik_kh2r1ZFyuDd22K4jA&oe=69669393",
-    ],
-    likes_count: 175000,
-    reposts_and_quotes_count: 2500,
-    replies_count: 1254,
+      "Hôm nay thử làm sushi tại nhà! Không ngon bằng tiệm nhưng cũng ổn 😅🍣",
+    media_urls: [demoImages[3], demoImages[4], demoImages[5], demoImages[6]],
+    likes_count: 1890,
+    reposts_and_quotes_count: 45,
+    replies_count: 52,
     created_at: "5h ago",
+    is_liked_by_auth: true,
+  },
+  {
+    id: 26592,
+    user: {
+      id: 119,
+      username: "travel_eats",
+      name: "Travel & Eats",
+      avatar_url:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=200",
+      isFollowing: true,
+      verified: false,
+    },
+    content:
+      "Pizza Napoli ở Ý quả là khác hẳn! Vỏ mỏng giòn, phô mai chảy tràn 🍕🇮🇹",
+    media_urls: [demoImages[7], demoImages[0]],
+    likes_count: 5620,
+    reposts_and_quotes_count: 234,
+    replies_count: 156,
+    created_at: "8h ago",
+    is_liked_by_auth: true,
+  },
+  {
+    id: 26593,
+    user: {
+      id: 120,
+      username: "banh_mi_queen",
+      name: "Bánh Mì Sài Gòn",
+      avatar_url:
+        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=200",
+      isFollowing: false,
+      verified: true,
+    },
+    content:
+      "Bánh mì thịt nướng ngon xuất sắc! Bánh giòn rụm, nhân đầy ắp 🥖🔥",
+    media_urls: [demoImages[2], demoImages[3], demoImages[4]],
+    likes_count: 4120,
+    reposts_and_quotes_count: 167,
+    replies_count: 93,
+    created_at: "1d ago",
+    is_liked_by_auth: false,
+  },
+  {
+    id: 26594,
+    user: {
+      id: 121,
+      username: "dessert_lover",
+      name: "Sweet Tooth",
+      avatar_url:
+        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=200",
+      isFollowing: true,
+      verified: false,
+    },
+    content:
+      "Matcha tiramisu tự làm! Vị đắng nhẹ hòa quyện với kem cheese béo ngậy 🍰💚",
+    media_urls: [demoImages[5], demoImages[6], demoImages[7], demoImages[1]],
+    likes_count: 2890,
+    reposts_and_quotes_count: 98,
+    replies_count: 71,
+    created_at: "1d ago",
     is_liked_by_auth: true,
   },
 ];
@@ -155,7 +148,7 @@ const FollowingFeed = () => {
 
   return (
     <div
-      className={`overflow-hidden bg-white pt-3 pb-14 ${isShowPadding && "pt-16"} dark:bg-[#181818]`}
+      className={`overflow-hidden bg-white pt-3 ${isShowPadding && "pt-16"} dark:bg-[#181818]`}
     >
       <div className="bg-white dark:bg-[#181818]">
         {DEMO_POSTS.map((post) => (
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -9,6 +9,131 @@ import UserAvatar from "@/components/users/UserAvatar";
 import { useCurrentUser } from "@/features/auth";
 
 import FollowerPreview from "@/components/users/FollowerPreview";
+import FeedItem from "@/components/Posts/FeedItem";
+
+const FAKE_POSTS = [
+  {
+    id: 30001,
+    user: {
+      id: 201,
+      username: "quanminh_dev",
+      name: "Quân Minh",
+      avatar_url: "https://i.pravatar.cc/400?img=12",
+      verified: false,
+      isFollowing: true,
+    },
+    content: `Cuối cùng cũng hoàn thành được cái project mà mình đã code suốt 3 tháng qua 🎉
+
+Từ những dòng code đầu tiên đến lúc deploy lên production, mỗi bug fix đều là một bài học quý giá. Cảm ơn team đã support nhiệt tình! 
+
+Giờ thì... nghỉ ngơi xứng đáng thôi! 😴`,
+    media_urls: [
+      "https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=800&h=600&fit=crop",
+      "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?w=800&h=600&fit=crop",
+    ],
+    likes_count: 456,
+    reposts_and_quotes_count: 23,
+    replies_count: 67,
+    created_at: "2h ago",
+    is_liked_by_auth: false,
+  },
+  {
+    id: 30002,
+    user: {
+      id: 201,
+      username: "quanminh_dev",
+      name: "Quân Minh",
+      avatar_url: "https://i.pravatar.cc/400?img=12",
+      verified: false,
+      isFollowing: true,
+    },
+    content: `Sáng nay đi uống cà phê ở quán quen, ngồi 1 mình nhìn người qua lại mà suy nghĩ về cuộc đời...
+
+Đôi khi chúng ta cứ chạy theo những thứ xa vời, mà quên mất hạnh phúc đơn giản ngay trước mắt.
+
+Một buổi sáng yên bình, một tách cà phê đậm đà, và khoảng thời gian để suy ngẫm - đó cũng là hạnh phúc rồi ☕️✨`,
+    media_urls: [
+      "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=800&h=600&fit=crop",
+    ],
+    likes_count: 892,
+    reposts_and_quotes_count: 45,
+    replies_count: 134,
+    created_at: "2026-01-10T08:30:00.000000Z",
+    is_liked_by_auth: true,
+  },
+  {
+    id: 30003,
+    user: {
+      id: 201,
+      username: "quanminh_dev",
+      name: "Quân Minh",
+      avatar_url: "https://i.pravatar.cc/400?img=12",
+      verified: false,
+      isFollowing: true,
+    },
+    content: `3 điều mình học được trong năm qua:
+
+1. Đừng so sánh process của mình với highlight reel của người khác
+2. Học cách nói "không" cũng quan trọng như học cách nói "có"  
+3. Sức khỏe tinh thần > Sự nghiệp thành công
+
+Năm mới, mình sẽ cố gắng sống chậm lại một chút, để cảm nhận nhiều hơn 🌱`,
+    media_urls: [],
+    likes_count: 1243,
+    reposts_and_quotes_count: 189,
+    replies_count: 201,
+    created_at: "2026-01-05T15:45:00.000000Z",
+    is_liked_by_auth: true,
+  },
+  {
+    id: 30004,
+    user: {
+      id: 201,
+      username: "quanminh_dev",
+      name: "Quân Minh",
+      avatar_url: "https://i.pravatar.cc/400?img=12",
+      verified: false,
+      isFollowing: true,
+    },
+    content: `Hôm nay làm việc remote ở Đà Lạt và mình nghĩ... đây chính là lý do tại sao mình yêu nghề này 💻🌲
+
+Có thể làm việc ở bất kỳ đâu, miễn là có wifi và laptop. View núi đồi, không khí mát mẻ, productivity tăng vọt!
+
+Ai đang plan workation thì Đà Lạt highly recommended nha! 🏔️`,
+    media_urls: [
+      "https://images.unsplash.com/photo-1519681393784-d120267933ba?w=800&h=600&fit=crop",
+      "https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?w=800&h=600&fit=crop",
+      "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?w=800&h=600&fit=crop",
+    ],
+    likes_count: 2156,
+    reposts_and_quotes_count: 312,
+    replies_count: 445,
+    created_at: "2025-12-28T10:20:00.000000Z",
+    is_liked_by_auth: false,
+  },
+  {
+    id: 30005,
+    user: {
+      id: 201,
+      username: "quanminh_dev",
+      name: "Quân Minh",
+      avatar_url: "https://i.pravatar.cc/400?img=12",
+      verified: false,
+      isFollowing: true,
+    },
+    content: `Mấy đứa bạn hỏi: "Sao mày lúc nào cũng tích cực vậy?"
+
+Thật ra không phải lúc nào mình cũng tích cực đâu. Cũng có những ngày down, những lúc muốn bỏ cuộc. Nhưng mình chọn cách chia sẻ những khoảnh khắc đẹp, những suy nghĩ tích cực để nhắc bản thân và mọi người rằng: cuộc sống vẫn có nhiều điều đáng trân trọng.
+
+Behind every positive post is a human being with real struggles. Let's be kind to each other ❤️`,
+    media_urls: [],
+    likes_count: 3421,
+    reposts_and_quotes_count: 567,
+    replies_count: 892,
+    created_at: "2025-12-20T18:00:00.000000Z",
+    is_liked_by_auth: true,
+  },
+];
 
 const Profile = () => {
   const { userId } = useParams();
@@ -127,7 +252,23 @@ const Profile = () => {
             Bài đăng lại
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="thread"></TabsContent>
+        <TabsContent value="thread">
+          {" "}
+          <div className={`overflow-hidden bg-white dark:bg-[#181818]`}>
+            <div className="bg-white dark:bg-[#181818]">
+              {FAKE_POSTS.map((post) => (
+                <div
+                  key={post.id}
+                  className="border-t border-gray-300 first-of-type:border-none dark:border-[#323030]"
+                >
+                  <div className="flex max-w-[640px] flex-col items-start overflow-hidden bg-white px-4 py-3 dark:bg-[#181818]">
+                    <FeedItem post={post} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </TabsContent>
         <TabsContent value="replies"></TabsContent>
         <TabsContent value="media"></TabsContent>
         <TabsContent value="reposts"></TabsContent>
